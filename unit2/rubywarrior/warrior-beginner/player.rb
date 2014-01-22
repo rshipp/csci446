d9bcb7e7fe722174d04d15dbd3759faca2dfab0b
george <rpubaddr0@gmail.com>
2014-01-21 22:10:39 -0700

    Completed levels 3-6.

--- unit2/rubywarrior/warrior-beginner/player.rb
+++ unit2/rubywarrior/warrior-beginner/player.rb
@@ -1,10 +1,35 @@
 class Player
+  MAX_HEALTH = 20
+  @health ||= MAX_HEALTH
+  @needsrest ||= false
+  @forward ||= false
+    
   def play_turn(warrior)
-    # add your code here
-    if warrior.feel.empty?
-        warrior.walk!
-    else
-        warrior.attack!
+    # Check health, reset needsrest if necessary.
+    if warrior.health == MAX_HEALTH
+      @needsrest = false
     end
+
+    # Check behind.
+    if warrior.feel(:backward).empty? && !@forward
+        warrior.walk! :backward
+    elsif warrior.feel(:backward).captive?
+        warrior.rescue! :backward
+    # Move forward.
+    elsif warrior.feel.empty? && warrior.health < MAX_HEALTH && !(warrior.health < @health)
+      @needsrest = true
+      warrior.rest!
+    elsif warrior.feel.empty? && (warrior.health < @health) && warrior.health <= 10
+      warrior.walk! :backward
+    elsif warrior.feel.empty?
+      @forward = true
+      warrior.walk!
+    elsif warrior.feel.captive?
+      warrior.rescue!
+    else
+      warrior.attack!
+    end
+
+    @health = warrior.health
   end
 end
