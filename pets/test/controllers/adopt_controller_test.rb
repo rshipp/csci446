9c1d031f1e8de2d3087d5819c0dac2ca01266917
george <rpubaddr0@gmail.com>
2014-02-05 03:57:06 -0700

    Adopt controller test.

--- pets/test/controllers/adopt_controller_test.rb
+++ pets/test/controllers/adopt_controller_test.rb
@@ -4,6 +4,9 @@ class AdoptControllerTest < ActionController::TestCase
   test "should get index" do
     get :index
     assert_response :success
+    assert_select '#columns #side a', :minimum => 2
+    assert_select '#main .entry', 2
+    assert_select 'h3', 'MyString'
   end
 
 end
