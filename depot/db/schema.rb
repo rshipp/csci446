a4f3b0317002e7a5d81f89a731e5ff2cd158a19a
george <rpubaddr0@gmail.com>
2014-02-12 01:14:55 -0700

    Fixing things.

--- depot/db/schema.rb
+++ depot/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140212065158) do
+ActiveRecord::Schema.define(version: 20140212071236) do
 
   create_table "carts", force: true do |t|
     t.datetime "created_at"
@@ -23,6 +23,7 @@ ActiveRecord::Schema.define(version: 20140212065158) do
     t.integer  "card_id"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.integer  "quantity"
   end
 
   create_table "products", force: true do |t|
