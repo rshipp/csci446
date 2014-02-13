e36b00df7911f949a201ec751962b0b2cdc79c54
george <rpubaddr0@gmail.com>
2014-02-12 20:32:48 -0700

    More user stuff.

--- depot/config/routes.rb
+++ depot/config/routes.rb
@@ -7,25 +7,28 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 Depot::Application.routes.draw do
-  get "admin/index"
-  get "sessions/new"
+  get 'admin' => 'admin#index'
+  controller :sessions do
+    get  'login' => :new
+    post 'login' => :create
+    delete 'logout' => :destroy
+  end
+
   get "sessions/create"
   get "sessions/destroy"
   resources :users
-
   resources :orders
-
   resources :line_items
-
   resources :carts
 
   get "store/index"
-  resources :products
+  resources :products do
+    get :who_bought, on: :member
+  end
 
   # The priority is based upon order of creation:
   # first created -> highest priority.
   # See how all your routes lay out with "rake routes".
-
   # You can have the root of your site routed with "root"
   root 'store#index', as: 'store'
   # ...
