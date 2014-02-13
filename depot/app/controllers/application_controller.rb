0e7054e410639e042f806b4a7b4af4c21d66f1a8
george <rpubaddr0@gmail.com>
2014-02-12 20:32:48 -0700

    Limiting access.

--- depot/app/controllers/application_controller.rb
+++ depot/app/controllers/application_controller.rb
@@ -7,7 +7,17 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 class ApplicationController < ActionController::Base
+  before_action :authorize
   # Prevent CSRF attacks by raising an exception.
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
+    # ...
+
+  protected
+
+    def authorize
+      unless User.find_by(id: session[:user_id])
+        redirect_to login_url, notice: "Please log in"
+      end
+    end
 end
--- depot/app/controllers/orders_controller.rb
+++ depot/app/controllers/orders_controller.rb
@@ -7,6 +7,7 @@
 # Visit http://www.pragmaticprogrammer.com/titles/rails4 for more book information.
 #---
 class OrdersController < ApplicationController
+  skip_before_action :authorize, only: [:new, :create]
   include CurrentCart
   before_action :set_cart, only: [:new, :create]
   before_action :set_order, only: [:show, :edit, :update, :destroy]
@@ -46,6 +47,7 @@ class OrdersController < ApplicationController
       if @order.save
         Cart.destroy(session[:cart_id])
         session[:cart_id] = nil
+        OrderNotifier.received(@order).deliver
         format.html { redirect_to store_url, notice: 
           'Thank you for your order.' }
         format.json { render action: 'show', status: :created,
