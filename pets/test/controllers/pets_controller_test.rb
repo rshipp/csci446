fe650d1090f4c5d0ba1526d2dc974c124d7e3669
george <rpubaddr0@gmail.com>
2014-02-19 03:51:04 -0700

    Done with chapter 7.

--- pets/test/controllers/pets_controller_test.rb
+++ pets/test/controllers/pets_controller_test.rb
@@ -3,6 +3,14 @@ require 'test_helper'
 class PetsControllerTest < ActionController::TestCase
   setup do
     @pet = pets(:one)
+    @update = {
+      name: 'one',
+      notes: 'notes',
+      breed: 'cat',
+      image_url: 'test.jpg',
+      age: 2,
+      shots: true
+    }
   end
 
   test "should get index" do
@@ -18,7 +26,8 @@ class PetsControllerTest < ActionController::TestCase
 
   test "should create pet" do
     assert_difference('Pet.count') do
-      post :create, pet: { age: @pet.age, breed: @pet.breed, image_url: @pet.image_url, name: @pet.name, notes: @pet.notes, shots: @pet.shots }
+      #post :create, pet: { age: @pet.age, breed: @pet.breed, image_url: @pet.image_url, name: @pet.name, notes: @pet.notes, shots: @pet.shots }
+      post :create, pet: @update
     end
 
     assert_redirected_to pet_path(assigns(:pet))
@@ -35,7 +44,8 @@ class PetsControllerTest < ActionController::TestCase
   end
 
   test "should update pet" do
-    patch :update, id: @pet, pet: { age: @pet.age, breed: @pet.breed, image_url: @pet.image_url, name: @pet.name, notes: @pet.notes, shots: @pet.shots }
+    #patch :update, id: @pet, pet: { age: @pet.age, breed: @pet.breed, image_url: @pet.image_url, name: @pet.name, notes: @pet.notes, shots: @pet.shots }
+    patch :update, id: @pet, pet: @update
     assert_redirected_to pet_path(assigns(:pet))
   end
 
--- pets/test/models/pet_test.rb
+++ pets/test/models/pet_test.rb
@@ -1,7 +1,55 @@
 require 'test_helper'
 
 class PetTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  fixtures :pets
+
+  test "pet attributes must not be empty" do
+    pet = Pet.new
+    assert pet.invalid?
+    assert pet.errors[:name].any?
+    assert pet.errors[:breed].any?
+    assert pet.errors[:image_url].any?
+    assert pet.errors[:notes].any?
+  end
+
+  test "pet age must be positive" do
+    pet = Pet.new(:name => 'one',
+                  :breed => 'two',
+                  :image_url => 'a.png',
+                  :shots => true,
+                  :notes => 'some')
+    pet.age = -1
+    assert pet.invalid?
+    assert_equal "must be greater than or equal to 1", pet.errors[:age].join('; ')
+
+    pet.age = 0
+    assert pet.invalid?
+    assert_equal "must be greater than or equal to 1", pet.errors[:age].join('; ')
+
+    pet.age = 1
+    assert pet.valid?
+  end
+
+  def new_pet(image_url)
+    Pet.new(:name => 'one',
+            :breed => 'two',
+            :image_url => image_url,
+            :age => 1,
+            :shots => true,
+            :notes => 'some')
+  end
+
+  test "image url" do
+    ok = %w{ fred.gif fred.jpg fred.png FRED.JPG FRED.Jpg
+             http://a.b.c/x/y/z/fred.gif }
+    bad = %w{ fred.doc fred.gif/more fred.gif.more }
+
+    ok.each do |name|
+      assert new_pet(name).valid?, "#{name} shouldn't be invalid"
+    end
+
+    bad.each do |name|
+      assert new_pet(name).invalid?, "#{name} shouldn't be valid"
+    end
+  end
 end
