27fda9876acb9338d684ebc0283463475a2d42c7
george <rpubaddr0@gmail.com>
2014-02-19 04:16:30 -0700

    Chapter 8 done.

--- pets/app/models/pet.rb
+++ pets/app/models/pet.rb
@@ -1,10 +1,13 @@
 class Pet < ActiveRecord::Base
-    validates :name, :notes, :image_url, :breed, presence: true
-    validates :age, numericality: {greater_than_or_equal_to: 1}
-    validates :name, uniqueness: true
-    validates :image_url, allow_blank: true, format: {
-        with:
-         %r{\.(gif|jpg|png)\Z}i,
-         message: 'must be a URL for GIF, JPG or PNG image.'
-    }
+  validates :name, :notes, :image_url, :breed, presence: true
+  validates :age, numericality: {greater_than_or_equal_to: 1}
+  validates :name, uniqueness: true
+  validates :image_url, allow_blank: true, format: {
+    with: %r{\.(gif|jpg|png)\Z}i,
+    message: 'must be a URL for GIF, JPG or PNG image.'
+  }
+
+  def self.latest
+    Pet.order(:updated_at).last
+  end
 end
