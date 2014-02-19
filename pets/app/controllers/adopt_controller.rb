905fffeff6e6331fcc80efefd575741185ed4836
george <rpubaddr0@gmail.com>
2014-02-19 04:04:22 -0700

    Added adopt controller sorting logic.

--- pets/app/controllers/adopt_controller.rb
+++ pets/app/controllers/adopt_controller.rb
@@ -1,5 +1,14 @@
 class AdoptController < ApplicationController
+  helper_method :sort_column, :sort_direction
   def index
-    @pets = Pet.order(:title)
+    @pets = Pet.order(sort_column + " " + sort_direction)
+  end
+
+  def sort_column
+    Pet.column_names.include?(params[:sort]) ? params[:sort] : "name"
+  end
+
+  def sort_direction
+    %w[asc desc].include?(params[:direction]) ? params[:direction] : "asc"
   end
 end
