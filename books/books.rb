4f1f20e65a6167a38cfb045c19061c7b407ff7f6
george <rpubaddr0@gmail.com>
2014-03-04 23:28:53 -0700

    Finished unit 8.

--- books/books.rb
+++ books/books.rb
@@ -1,14 +1,24 @@
-#!/usr/bin/ruby
+#!/usr/bin/env ruby
 
 require 'rack'
+require 'csv'
 
 class BooksApp
-  def initialize()
+  def initialize
     # Set up variables that will be needed later.
-    
+    @books = []
+    f = File.open("books.csv", "r")
+    f.each do |line|
+      l = []
+      (CSV.parse_line line).each do |row|
+        l.push row.strip
+      end
+      @books.push l
+    end
+    @options = ['title', 'author', 'language', 'year']
   end
 
-  def call(env)
+  def call env
     # Create request and response objects.
     request = Rack::Request.new(env)
     response = Rack::Response.new
@@ -18,16 +28,15 @@ class BooksApp
     
     case env["PATH_INFO"]
       when /.*?\.css/
-        # Serve up a css file.
-        # Remove leading /
+        # Serve up a css file. Remove leading /
         file = env["PATH_INFO"][1..-1]
         return [200, {"Content-Type" => "text/css"}, [File.open(file, "rb").read]]
       when /\/form.*/
         # Serve up the form.
-        render_form(request, response)
+        render_form request, response
       when /\/list.*/
         # Serve up a list response.
-        render_list(request, response)
+        render_list request, response
       else
         [404, {"Content-Type" => "text/plain"}, ["Error 404!"]]
     end
@@ -37,15 +46,59 @@ class BooksApp
     response.finish
   end
 
+
   # /form
   def render_form req, response
-    response.write "Form"
+    page = []
+
+    page.push "<form method='GET' action='/list'>"
+    page.push "\t<select name='sort'>"
+    @options.each do |option|
+      page.push "\t\t<option>#{option}</option>"
+    end
+    page.push "\t</select>"
+    page.push "\t<button type='submit'>Display List</button>"
+    page.push "</form>"
+
+    page.each do |line|
+      response.write "\t\t" + line + "\n"
+    end
   end
 
   # /list
   def render_list req, response
     sort = req.GET["sort"]
-    response.write "List"
+    page = []
+
+    if @options.include? sort
+      page.push "<h2>Sorted by #{sort}</h2>"
+      page.push "<table cellborder=1>"
+      page.push "\t<tr>"
+      page.push "\t\t<th>Rank</th>"
+      page.push "\t\t<th>Title</th>"
+      page.push "\t\t<th>Author</th>"
+      page.push "\t\t<th>Language</th>"
+      page.push "\t\t<th>Year</th>"
+      page.push "\t\t<th>Copies</th>"
+      page.push "\t</tr>"
+
+      (@books.sort_by { |e| e[@options.index sort] }).each do |book|
+        page.push "\t<tr>"
+        page.push "\t\t<td>#{(@books.index book) + 1}</td>"
+        book.each do |field|
+          page.push "\t\t<td>#{field}</td>"
+        end
+        page.push "\t</tr>"
+      end
+
+      page.push "</table>"
+    else
+      page.push "Parameter sort=#{sort} is not allowed."
+    end
+
+    page.each do |line|
+      response.write "\t\t" + line + "\n"
+    end
   end
 end
 
