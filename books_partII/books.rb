2cbda08851d377180d9670ce00bc567618e18451
george <rpubaddr0@gmail.com>
2014-03-20 23:26:47 -0600

    Basic functionality in Sinatra.

--- books_partII/books.rb
+++ books_partII/books.rb
@@ -1,82 +1,33 @@
 #!/usr/bin/env ruby
 
-require 'rack'
-require 'csv'
-require 'erb'
-require 'sqlite3'
 require 'data_mapper'
 require 'sinatra'
 require_relative 'book'
 
-class BooksApp
-  attr_accessor :options
-  include ERB::Util
+DataMapper.setup(:default, "sqlite://#{Dir.pwd}/books.sqlite3.db")
 
-  def initialize
-    # Set up variables that will be needed later.
-    @db = SQLite3::Database.new "books.sqlite3.db"
-    @options = ['rank', 'title', 'author', 'language', 'year']
-  end
+books = []
+Book.all.each { |book| books.push [book.id, book.title, book.author, book.language, book.published] }
+puts books
+options = ['rank', 'title', 'author', 'language', 'year']
 
-  def call env
-    # Create request and response objects.
-    request = Rack::Request.new(env)
-    response = Rack::Response.new
-
-    # Include the header.
-    File.open("header.html", "r") { |head| response.write(head.read) }
-    
-    case env["PATH_INFO"]
-      when /.*?\.css/
-        # Serve up a css file. Remove leading /
-        file = env["PATH_INFO"][1..-1]
-        return [200, {"Content-Type" => "text/css"}, [File.open(file, "rb").read]]
-      when /\/form.*/
-        # Serve up the form.
-        render_form request, response
-      when /\/list.*/
-        # Serve up a list response.
-        render_list request, response
-      else
-        [404, {"Content-Type" => "text/plain"}, ["Error 404!"]]
-    end
-
-    # Include the footer.
-    File.open("footer.html", "r") { |foot| response.write(foot.read) }
-    response.finish
-  end
-
-
-  # /form
-  def render_form req, response
-    template = File.open('form.html.erb', 'r').read
-    response.write(ERB.new(template).result binding)
-  end
-
-  # /list
-  def render_list req, response
-    sort = req.GET["sort"]
-    if @options.include? sort
-      template = File.open('list.html.erb', 'r').read
-      books = []
-      table = @db.execute("select * from books")
-      (table.sort_by { |e| e[@options.index sort] }).each do |book|
-        col = []
-        book.each do |field|
-          col.push field
-        end
-        books.push col
-      end
-      response.write(ERB.new(template).result binding)
-    else
-      response.write "\t\tParameter sort=#{sort} is not allowed.\n"
-    end
-  end
+get '/form' do
+  erb :base, locals: {page: 'form', options: options, books: nil, sort: nil}
 end
 
-
-Signal.trap('INT') {
-  Rack::Handler::WEBrick.shutdown
-}
-
-Rack::Handler::WEBrick.run BooksApp.new, :Port => 8080
+get '/list' do
+  sort = request.GET["sort"]
+  if options.include? sort
+    table = []
+    (books.sort_by { |e| e[options.index sort] }).each do |book|
+      col = []
+      book.each do |field|
+        col.push field
+      end
+      table.push col
+    end
+    return erb :base, locals: {page: 'list', options: options, books: table, sort: sort}
+  else
+    return "Parameter sort=#{sort} is not allowed.\n"
+  end
+end
